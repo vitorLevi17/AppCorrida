1ef6282511429765aded21870de8655207798e19
Levi Santos <85260420+vitorLevi17@users.noreply.github.com>
2025-02-13 11:22:26 -0300

    Update wsgi.py

--- AppCorridas/setup/wsgi.py
+++ AppCorridas/setup/wsgi.py
@@ -1,16 +1,15 @@
-"""
-WSGI config for setup project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
+# This file contains the WSGI configuration required to serve up your
+# Django app
 import os
+import sys
 
+# Add your project directory to the sys.path
+settings_path = '/home/AppCorridas/appcorridas.pythonanywhere.com/AppCorridas'
+sys.path.insert(0, settings_path)
+
+# Set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'setup.settings'
+
+# Set the 'application' variable to the Django wsgi app
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
-
 application = get_wsgi_application()
