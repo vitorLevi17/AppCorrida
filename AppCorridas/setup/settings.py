affe751d342f59681fbe90236594b78426e067c9
Levi Santos <85260420+vitorLevi17@users.noreply.github.com>
2025-02-13 11:12:18 -0300

    Update settings.py

--- AppCorridas/setup/settings.py
+++ AppCorridas/setup/settings.py
@@ -117,9 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
