e078f7b8f56f9afd1bb62526a18e17d6db8228df
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2025-02-17 15:32:04 -0300

    Novo estilo
    Mais dinamico e com cores que remetem a area de atuação

--- AppCorridas/manage.py
+++ AppCorridas/manage.py
@@ -3,8 +3,9 @@
 import os
 import sys
 # 3-Enviar para os aplicativos e receber os preços
-# estilos
-# hospedar -> anuncios 1/2 ->
+# estilos comparar.html
+# hospedar
+# anuncios
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
